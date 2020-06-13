eb2d6ef3801d90fd32c1b6a2a1f5a2e4c5bedad0
clark-lindsay <clarklindsay96@gmail.com>
2020-06-12 23:49:48 -0400

    Adjusted algorithm to be O(n) time complexity, and removed the requirement that the answer returned must be deterministic.

--- src/twosum.py
+++ src/twosum.py
@@ -1,7 +1,8 @@
 def two_sum(nums, target):
-    for firstIndex, num1 in enumerate(nums):
-        for secondIndex, num2 in enumerate(nums):
-            if (num1 + num2 == target):
-                if (firstIndex != secondIndex):
-                    return [firstIndex, secondIndex]
+    targetSumDict = {}
+    for index, num in enumerate(nums):
+        targetSumDict[num] = index
+    for index, num in enumerate(nums):
+        if (targetSumDict.__contains__(target - num)):
+            return [index, targetSumDict.get(target - num)]
     return []
